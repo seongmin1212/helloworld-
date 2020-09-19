5f27399a6218ea5dfd3cb52096db67d1fa0f6e59
intel-PC\intel <omhjsh2002@naver.com>
2020-09-19 11:00:26 +0900

    자바 프로그래밍 연습

---
+++ JAVApractice/Chapter2/pratice/src/pratice/DoubleEx.java
@@ -0,0 +1,16 @@
+package pratice;
+
+public class DoubleEx {
+
+	public static void main(String[] args) {
+		var dNum = 3.14;
+		float fNum = 3.14F;
+		
+		System.out.println(dNum);
+		System.out.println(fNum);
+		
+		var num = 10;
+		System.out.println(num);
+	}
+
+}
---
+++ JAVApractice/Chapter2/pratice/src/pratice/VariableEx.java
@@ -0,0 +1,19 @@
+package pratice;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		
+		int num;
+		num = 10;
+		
+		System.out.println(num);
+		
+		int level = 20;
+		System.out.println(level);
+		
+		int ns;
+		int numberOfStudent;
+	}
+
+}
